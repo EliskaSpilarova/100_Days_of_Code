ece15f21aad96768a94e167abff51f43f802e126
Eliška Špilarová <164064165+EliskaSpilarova@users.noreply.github.com>
2025-01-23 18:49:05 +0100

    Section 1.py

--- Section 1.py
+++ Section 1.py
@@ -1,4 +1,18 @@
-# String manipulation
+ # String manipulation
 print("Hello world!\nHello world!\nHello world!")
 
 print("Hello " + input("Write your name"))
+
+# Variables
+name = input("What is your name?")
+print(len(name))
+
+username = input("Write your name.")
+length = len(username)
+print(length)
+
+# Band Name Generator Project
+print("Hi user!")
+city = input("List the name of your city\n")
+name = input("List the name of your pet\n")
+print(city, name)
