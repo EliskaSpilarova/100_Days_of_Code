08dd0c61f21f16fe8483b49a512a976c0daf63f3
Eliška Špilarová <164064165+EliskaSpilarova@users.noreply.github.com>
2025-01-25 14:49:51 +0100

    Section 5

---
+++ Section 5.py
@@ -0,0 +1,41 @@
+# Highest number
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+
+print(highest_score)
+
+# Gauss Challenge 
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
+
+# Random password generator
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = ""
+for char in range(0, nr_letters):
+     password += random.choice(letters)
+
+for char in range(0, nr_symbols):
+    password += random.choice(symbols)
+
+for char in range(0, nr_numbers):
+    password += random.choice(numbers)
+
+print(password)
+
+
